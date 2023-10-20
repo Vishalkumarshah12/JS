702afe37b09cc144d2a7d0515774827a0f0cece1
Vishalkumarshah12 <148333215+Vishalkumarshah12@users.noreply.github.com>
2023-10-20 07:19:18 +0000

    forEach_map_filter

---
+++ 01_basics/forEach_map_filter.js
@@ -0,0 +1,16 @@
+
+              //ForEach
+
+// const conding = ["java","Python","Js","ruby"]
+
+// forEach not return value
+// const value = conding.forEach(function(item){
+//     console.log(item)
+//     return item
+// })
+// console.log(value);
+
+
+
+       
+          //
