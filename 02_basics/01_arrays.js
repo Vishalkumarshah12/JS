e0a4ba3e96a6717f1c12da793978ecde2e1ce2f2
Vishalkumarshah12 <148333215+Vishalkumarshah12@users.noreply.github.com>
2023-10-21 11:45:22 +0000

    Arrays

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,39 @@
+// Array
+
+const myArr = [0, 1, 2, 3, 4, 5, ]
+const myHeors = ["Ironman", "Spirdman","Superman",'Flash']
+
+const myArr2 = new Array(1, 2, 3, 4)
+// console.log(myArr[1]);
+
+// Array methods
+
+// myArr.push(6) // add values in array
+// myArr.push(7) // add values in array
+// myArr.pop() // last value remove of arrays
+
+// myArr.unshift(9)
+// myArr.shift()
+
+// console.log(myArr.includes(9));
+// console.log(myArr.indexOf(3));
+
+const newArr = myArr.join()
+
+// console.log(myArr);
+// console.log( newArr);
+// console.log( typeof newArr);
+
+
+// slice "or" splice
+
+console.log("A", myArr);
+
+const myn1 = myArr.slice(1, 3) //it does not manipuled the original values in arrays
+
+console.log(myn1);
+console.log("B", myArr);
+
+const myn2 = myArr.splice(1, 3) //it manipuled the original values in arrays
+console.log("C", myArr);
+console.log(myn2);
