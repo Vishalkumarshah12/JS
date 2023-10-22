ecaa5e7cc259f3e4d673e29a1b3c0e8bc38a4abe
Vishalkumarshah12 <148333215+Vishalkumarshah12@users.noreply.github.com>
2023-10-22 06:53:38 +0000

    objects

--- 02_basics/01_arrays.js
+++ 02_basics/01_arrays.js
@@ -8,7 +8,7 @@ const myArr2 = new Array(1, 2, 3, 4)
 
 // Array methods
 
-// myArr.push(6) // add values in array
+// myArr.push(6) // add values in array 
 // myArr.push(7) // add values in array
 // myArr.pop() // last value remove of arrays
 
@@ -21,19 +21,19 @@ const myArr2 = new Array(1, 2, 3, 4)
 const newArr = myArr.join()
 
 // console.log(myArr);
-// console.log( newArr);
+// console.log( newArr); // convert into string
 // console.log( typeof newArr);
 
 
 // slice "or" splice
 
-console.log("A", myArr);
+// console.log("A", myArr);
 
-const myn1 = myArr.slice(1, 3) //it does not manipuled the original values in arrays
+// const myn1 = myArr.slice(1, 3) //it does not manipuled the original values in arrays
 
-console.log(myn1);
-console.log("B", myArr);
+// console.log(myn1);
+// console.log("B", myArr);
 
-const myn2 = myArr.splice(1, 3) //it manipuled the original values in arrays
-console.log("C", myArr);
-console.log(myn2);
+// const myn2 = myArr.splice(1, 3) //it manipuled the original values in arrays
+// console.log("C", myArr);
+// console.log(myn2);
