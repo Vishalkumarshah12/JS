a54f046f9e23a73d316d02b04185826cec8a199b
Vishalkumarshah12 <148333215+Vishalkumarshah12@users.noreply.github.com>
2023-10-22 08:58:16 +0000

    function

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -35,5 +35,37 @@ function LoginUserMessage(username = "sam"){
     return `${username} just logged in`
 }
 
-console.log(LoginUserMessage("Bishal"))
+// console.log(LoginUserMessage("Bishal"))
 // console.log(LoginUserMessage("Vishal"))
+
+
+function calculateCardPrice(val1,val2,...num1){
+   return num1 
+}
+
+// console.log(calculateCardPrice(200, 400, 500, 2000))
+
+const user = {
+    username: "Bishal",
+    prices: 199
+}
+
+function handleObject(anyobject){
+      console.log(`Username is ${anyobject.username} and price is ${anyobject.price}`);
+}
+
+// handleObject(user)
+handleObject({
+    username: "sam",         //Passing the object in function
+    price: 399
+})       
+
+
+const myNewArray = [200, 400, 100, 600]
+
+function returnSecondValue(getArray){
+       return getArray[1]
+}
+
+// console.log(returnSecondValue(myNewArray))
+console.log(returnSecondValue([200, 400, 500, 1000]))
