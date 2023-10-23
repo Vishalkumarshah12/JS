18cf767716ea92a42b99b6dd6e285c7532c2e968
Vishalkumarshah12 <148333215+Vishalkumarshah12@users.noreply.github.com>
2023-10-23 13:36:21 +0000

    loops

---
+++ 04_control_flow/for_in.js
@@ -0,0 +1,17 @@
+const myObject = {
+    js:'javascript',
+    cpp:'C++',
+    rb:'ruby',
+    swift:"swift by apple"
+}
+
+for (const key in myObject) {
+    
+    // console.log(`${key} shortcut for ${[key]}`);
+}
+
+const programming = ['js','cpp','py','java','rb']
+
+for (const key in programming) {
+     console.log(key);
+}
---
+++ 04_control_flow/for_of_and_map.js
@@ -0,0 +1,41 @@
+// for of
+
+// ["","",""]
+// [{},{},{}]
+
+const arr = [1, 2, 3, 4, 5]
+
+for (const num of arr) {
+    // console.log(num);
+}
+
+const greetings = "Hello World"
+
+for (const item of greetings) {
+    // console.log(`Each char is ${item}`);
+}
+
+// Maps for unik values
+
+const map = new Map()
+map.set('IN','India')
+map.set('USA','United states of America')
+map.set('FR','France')
+
+// console.log(map);
+
+for (const [key, values] of map) {
+    //  console.log(key ,':-',values);
+}
+
+
+// for of is not work in Objects 
+
+// const myObject = {
+//     'game1':'NFS',
+//     'game2':'Spiderman'
+// }
+
+// for (const [key, values] of myObject) {
+//       console.log(key,values);
+// }
