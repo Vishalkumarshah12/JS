da354fc75f24c55bfc0fbdc3c07331cc699a5384
Vishalkumarshah12 <148333215+Vishalkumarshah12@users.noreply.github.com>
2023-10-31 11:59:41 +0000

    OOP

---
+++ 10_classes_and_oop/oop.js
@@ -0,0 +1,37 @@
+
+//   Object literal
+const user = {
+    username: "Vishal",
+    loginCount: 8,
+    signedIn: true,
+
+    getUserDetails: function(){
+        // console.log(`Got user details from database`);
+        // console.log(`Username: ${this.username}`);
+        // console.log(this);
+    }
+}
+
+// console.log(user.username);
+// console.log(user.getUserDetails());
+// console.log(this);
+
+
+
+function User(username, loginCount, isLoggedIn){
+    this.username = username;
+    this.loginCount = loginCount;
+    this.isLoggedIn = isLoggedIn
+
+    this.greeting = function(){
+        console.log(`Welcome to ${this.username}`);
+    }
+
+    return this
+}
+
+const userOne = new User("Vishal", 12, true)
+const userTwo = new User("Shivam", 11, false)
+
+console.log(userOne.constructor);
+// console.log(userTwo);
